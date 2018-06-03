15401b3c0baa1ff8d228c4ab62e18117d2a3304b
odeliaevgi <37341260+odeliaevgi@users.noreply.github.com>
2018-06-03 13:49:50 +0300

    GREEN: added more  functions and tests and succesfuly passed

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -5,7 +5,17 @@ function sum (a, b) {
   // your code to make the test pass goes here ...
 }
 
+function sub (a, b) {
+	return a-b;
+ 
+}
+
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+
+test('sub should return the sub of two numbers', function (t) {
+  t.equal(0, sub(2, 2)); // make this test pass by completing the add function!
   t.end();
 });
